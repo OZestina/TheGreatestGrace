c134cd6e4b6f7615d2d258baf7429bf64678c4a1
Zestina5 <zestina.oh@gmail.com>
2020-10-06 00:55:21 +0900

    K번째 문제_프로그래머스

---
+++ programmers/K.py
@@ -0,0 +1,13 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i = command[0]
+        j = command[1]
+        k = command[2]
+        
+        wtf = array[i-1:j]
+        wtf.sort()
+        answer.append(wtf[k-1])
+    print(answer)
+    return answer
