d172f7f9389bdffa9f42e9b5dcd4740d623d320d
Zestina5 <72402916+Zestina5@users.noreply.github.com>
2020-12-28 00:48:37 +0900

    Update sum_of_two_integers.py

--- programmers/sum_of_two_integers.py
+++ programmers/sum_of_two_integers.py
@@ -1,16 +1,4 @@
 #두 정수 사이의 합
 def solution(a, b):
-    answer = 0
-    
-    if a == b:
-        answer = a
-    else:
-        if a > b:
-            c = b
-            b = a
-            a = c
-        while b+1-a > 0:
-            answer = answer + a
-            a = a+1
-
-    return answer
+    if a > b: a,b = b,a
+    return sum(range(a,b+1))
