e327974f7542ddd1071d6b2a33f1b44b82c79b03
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-02-08 01:03:51 +0900

    Update *0208_(x,n).py

--- programmers/*0208_(x,n).py
+++ programmers/*0208_(x,n).py
@@ -14,3 +14,16 @@ def solution(x, n):
     return answer
     
 # if 부분에서 pass로 하는게 맞는지 확인 필요 (0이 자연수라고 할 경우)
+
+# range를 찾아서 새로 고침!
+
+def solution(x,n):
+    answer = []
+    for i in range(n):
+        answer.append(x*i + x)
+    return answer
+
+# (멋지게) 한 줄로는 이렇게 한대요
+
+def solution(x,n):
+    return [x*i + x for i in range(n)]
