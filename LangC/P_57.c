d83be80e2ab8aab55d133f68614af3d6ae3aa5fe
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-12 10:34:42 +0900

    Update P_57.c

--- LangC/P_57.c
+++ LangC/P_57.c
@@ -6,7 +6,7 @@ int main(void)
 	printf("%d+%d=%d\n", num1, num2, num1+num2);
 	printf("%d-%d=%d\n", num1, num2, num1-num2);
 	printf("%d*%d=%d\n", num1, num2, num1*num2);
-	printf("%d/%d의 몫=%d (변수가 int로 선언돼서 소숫점 저장 못함)\n" , num1, num2, num1/num2);
+	printf("%d/%d의 몫=%d (%d가 10진수의 정수를 출력하는 서식문자여서 소숫점 생략)\n" , num1, num2, num1/num2);
 	printf("%d/%d의 나머지=%d\n", num1, num2, num1%num2);
 	return 0;
 }
