97a7097656a3af254d300f5df5f2bd82f9af31c6
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-12 11:35:06 +0900

    Create P_62.c

---
+++ LangC/P_62.c
@@ -0,0 +1,17 @@
+//OperatorFour.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num1=12, num2 = 12;
+	printf("num1: %d\n", num1);
+	printf("num1++: %d\n", num1++);
+	printf("num1: %d\n\n", num1);
+
+	printf("num2: %d\n", num2);
+	printf("++num2: %d\n", ++num2);
+	printf("num2: %d\n\n", num2);
+
+	return 0;
+}
