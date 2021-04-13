86def67866b0623d53a3192b72dda973de85e1ed
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-14 03:21:05 +0900

    Update _Operator.c

--- LangC/_Operator.c
+++ LangC/_Operator.c
@@ -1,6 +1,29 @@
 //연산자(Operator)
 
 /*
+0. sizeof 연산자
+//변수, 상수 및 "자료형의 이름"을 피연산자로 연산 가능하다!
+*/
+#include <stdio.h>
+int main()
+{
+	char ch = 9;
+	int inum = 1052;
+	double dnum = 3.1415;
+	printf("변수 ch의 크기: %d \n", sizeof(ch));
+	printf("변수 inum의 크기: %d \n", sizeof(inum));
+	printf("변수 dnum의 크기: %d \n", sizeof(dnum));
+
+	printf("char의 크기: %d \n", sizeof(char));
+	printf("int의 크기: %d \n", sizeof(int));
+	printf("long의 크기: %d \n", sizeof(long));
+	printf("long long의 크기: %d \n", sizeof(long long));
+	printf("float의 크기: %d \n", sizeof(float));
+	printf("double의 크기: %d \n", sizeof(double));
+	return 0;
+}
+
+/*
 1. 복합 대입 연산자
 a=a+b 를 간단히-> a+=b
 a=a-b 를 간단히-> a-=b
