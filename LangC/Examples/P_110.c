97b52a0ab344ac8e93db7d7adddd89bd5f0c9b25
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-14 16:21:20 +0900

    Create P_110.c

---
+++ LangC/Examples/P_110.c
@@ -0,0 +1,21 @@
+//CharShortBaseAdd.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	char num1=1, num2=2, result1=0;
+	short num3=300, num4=400, result2=0;
+
+	printf("size of num1 & num2: %d, %d \n", sizeof(num1), sizeof(num2));
+	printf("size of num3 & num4: %d, %d \n", sizeof(num3), sizeof(num4));
+
+	printf("size of char add: %d \n", sizeof(num1+num2));
+	printf("size of short add: %d \n", sizeof(num3+num4));
+
+	result1=num3+num4; //error값
+	result2=num1+num2; //진행 가능
+	printf("result1 & result2: %d, %d \n", result1, result2);
+	printf("size of result1 & result2: %d, %d \n", sizeof(result1), sizeof(result2));
+	return 0;
+}
