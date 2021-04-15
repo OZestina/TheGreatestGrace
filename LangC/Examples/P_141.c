5d0c0ddfadfd9c18c04a23dd94a32831d25c2e7b
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-16 04:36:42 +0900

    Create P_141.c

---
+++ LangC/Examples/P_141.c
@@ -0,0 +1,17 @@
+//UsingPG.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	double d1=1.23e-3;	//0.00123
+	double d2=1.23e-4;	//0.000123
+	double d3=1.23e-5;	//0.0000123
+	double d4=1.23e-6;	//0.00000123
+
+	printf("%g \n", d1);	//%f
+	printf("%g \n", d2);	//%f
+	printf("%g \n", d3);	//%e
+	printf("%g \n", d4);	//%e
+	return 0;
+}
