9014544ec9eac5169a9a9d448017639d1055748a
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-17 14:01:30 +0900

    Update P_149.c

--- LangC/Examples/P_149.c
+++ LangC/Examples/P_149.c
@@ -13,3 +13,18 @@ int main(void)
 	}
 	return 0;
 }
+
+//반복의 대상이 한 줄일 경우 중괄호 생략 가능
+//num++은 라인 이후에 +1되는거니까 이렇게도 쓸 수 있구나!
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num=0;
+
+	while(num<5)
+		printf("Hello world! %d \n", num++);
+	
+	return 0;
+}
