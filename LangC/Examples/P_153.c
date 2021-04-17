53c89769111673c5cac1de3088d53facf991a30f
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-17 14:46:17 +0900

    Create P_153.c

---
+++ LangC/Examples/P_153.c
@@ -0,0 +1,17 @@
+//NineNinedan.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int dan=0, num=1;
+	printf("몇 단? ");
+	scanf("%d", &dan);
+
+	while (num<10)
+	{
+		printf("%d x %d = %d \n", dan, num, dan*num);
+		num++;
+	}
+	return 0;
+}
