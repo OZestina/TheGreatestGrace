27eb4adba5149a3107ef743945878aec49526c48
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-20 01:18:37 +0900

    Create P_168.c

---
+++ LangC/Examples/P_168.c
@@ -0,0 +1,17 @@
+//TwoToNineForVer.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int cur, is;
+
+	for(cur=2; cur<10; cur++)
+	{
+		for(is=1; is<10; is++)
+			printf("%d x %d = %d \n",cur, is, cur*is);
+		
+		printf("\n");
+	}
+	return 0;
+}
