8b04a52f6e9d466acfd06007066ac5d1a65d103a
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-19 23:45:58 +0900

    Create P_165.c

---
+++ LangC/Examples/P_165.c
@@ -0,0 +1,17 @@
+//AddToNum.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int total = 0;
+	int i, num;
+	printf("0부터 num까지의 덧셈, num은? ");
+	scanf("%d", &num);
+
+	for(i=0; i<num+1; i++)
+		total += i;
+	printf("0부터 %d까지 덧셈결과: %d\n", num, total);
+
+	return 0;
+}
