b0e23b19c733ff4de7996ce7f04d6c254d7cb37c
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-20 01:35:29 +0900

    Update 07-4-2.c

--- LangC/Practices/07-4-2.c
+++ LangC/Practices/07-4-2.c
@@ -16,3 +16,26 @@ int main(void)
 	printf("%d의 팩토리얼 값은 %d입니다. \n", num, total);
 	return 0;
 }
+
+
+//2nd try
+//변수 2개만 가지고 해봄
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	int total=1;
+	
+	printf("팩토리얼 값을 계산할 정수를 입력하세요: ");
+	scanf("%d", &num);
+
+	for (; num>0; num--)
+	{
+		total *= num;
+	}
+
+	printf("팩토리얼 값은 %d입니다. \n", total);
+	return 0;
+}
