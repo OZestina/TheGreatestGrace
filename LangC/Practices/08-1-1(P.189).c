96d59df2186aebaf606856f4ab3e0735e9409373
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-20 04:56:30 +0900

    Update 08-1-1(P.189).c

--- LangC/Practices/08-1-1(P.189).c
+++ LangC/Practices/08-1-1(P.189).c
@@ -9,3 +9,19 @@ int main(void)
 
 	return 0;
 }
+
+
+//또 다른 방법 (line 7 변형)
+// !연산자는 참 <-> 거짓 변경. 나머지로 0이 반환될 때 if문의 조건이 참이 되게 하는 방법.
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	for (num=1; num<100; num++)
+		if ( !(num%7) || !(num%9) )
+			printf("%d \n", num);
+
+	return 0;
+}
