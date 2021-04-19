8ca8d748c2bc77841abae80e3fa3c879c7854c32
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-20 04:29:35 +0900

    Create P_185.c

---
+++ LangC/Examples/P_185.c
@@ -0,0 +1,28 @@
+//CalTwo.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int opt;
+	double num1, num2;
+	double result;
+
+	printf("1.덧셈 2.뺄셈 3.곱셈 4.나눗셈\n");
+	printf("당신의 선택은? ");
+	scanf("%d", &opt);
+	printf("두 개의 실수 입력: ");
+	scanf("%lf %lf", &num1, &num2);
+
+	if(opt==1)
+		result = num1+num2;
+	else if(opt==2)
+		result = num1-num2;
+	else if(opt==3)
+		result = num1*num2;
+	else
+		result = num1/num2;
+	
+	printf("결과: %f\n", result);
+	return 0;
+}
