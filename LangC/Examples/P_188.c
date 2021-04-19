679040b7aafbe41cf3e112968ddc164330c21617
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-20 04:36:02 +0900

    Create P_188.c

---
+++ LangC/Examples/P_188.c
@@ -0,0 +1,14 @@
+//ThreeOperand.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num, abs;
+	printf("정수 입력: ");
+	scanf("%d", &num);
+
+	abs = num>0 ? num : num*(-1);
+	printf("절대값: %d \n",abs);
+	return 0;
+}
