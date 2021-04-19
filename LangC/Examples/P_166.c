8a0f4a00a5cacf6aa3289708994868b93a575b21
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-19 23:51:02 +0900

    Create P_166.c

---
+++ LangC/Examples/P_166.c
@@ -0,0 +1,20 @@
+//RealMean.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	double total=0.0;
+	double input = 0.0;
+	int num=0;
+
+	for ( ; input>=0.0; )
+	{
+		total += input;
+		printf("실수 입력(minus to quit) : ");
+		scanf("%lf", &input);
+		num++;
+	}
+	printf("평균: %f \n", total/(num-1));
+	return 0;
+}
