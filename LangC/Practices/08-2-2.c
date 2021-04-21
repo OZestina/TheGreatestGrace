e509772366787a0ccaedbe43a918cda6dc5b4bb3
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-22 00:05:33 +0900

    Update 08-2-2.c

--- LangC/Practices/08-2-2.c
+++ LangC/Practices/08-2-2.c
@@ -20,3 +20,25 @@ int main(void)
 
 //2nd try with Continue & Break
 
+#include <stdio.h>
+
+int main(void)
+{
+	int a=1, z=1;
+
+	while(1)
+	{
+		for(z=1; z<10; z++)
+		{
+			if(a+z != 9)
+				continue;
+			else
+				printf("A: %d, Z: %d\n", a, z);
+		}
+		a++;
+		if(a>9)
+			break;
+	}
+
+	return 0;
+}
