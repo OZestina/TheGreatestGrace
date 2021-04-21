485541e570f5ce18620c28129c15fb2df2b842d1
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-22 00:50:31 +0900

    Create P_201.c

---
+++ LangC/Examples/P_201.c
@@ -0,0 +1,30 @@
+//GoToBasic.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	printf("자연수 입력: ");
+	scanf("%d", &num);
+
+	if(num==1)
+		goto ONE;
+	else if(num==2)
+		goto TWO;
+	else
+		goto OTHER;
+
+ONE:
+	printf("1을 입력하셨습니다!\n");
+	goto END;
+TWO:
+	printf("2를 입력하셨습니다!\n");
+	goto END;
+OTHER:
+	printf("3 혹은 다른 값을 입력하셨군요!\n");
+
+END:
+	printf("안녕히계세요!\n");
+	return 0;
+}
