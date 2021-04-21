50bea206eee51c004b0ecb7f1407c63b2967812c
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-22 00:16:44 +0900

    Update 08-2-1(P.193).c

--- LangC/Practices/08-2-1(P.193).c
+++ LangC/Practices/08-2-1(P.193).c
@@ -38,3 +38,27 @@ int main(void)
 	}
 	return 0;
 }
+
+
+//답지
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	for(i=1; i<10; i++)
+	{
+		if(i%2!=0)
+			continue;
+		
+		for(j=1; j<10; j++)
+		{
+			if(i<j)
+				break;
+			printf("%d x %d = %d\n", i, j, i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
