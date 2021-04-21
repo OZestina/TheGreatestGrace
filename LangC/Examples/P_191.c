cd068044ab994690d4cd2aac9d30c35c0f17c011
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-21 23:31:55 +0900

    Create P_191.c

---
+++ LangC/Examples/P_191.c
@@ -0,0 +1,20 @@
+//ExceptMul2Mul3.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int num;
+	printf("start! ");
+
+	for (num=1; num<20; num++)
+	{
+		if(num%2==0 || num%3 ==0)
+			continue;
+		printf("%d ", num);
+	}
+
+	printf("end! \n");
+
+	return 0;
+}
