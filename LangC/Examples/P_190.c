49fe392211b4d9b8b733cf2b25bec0feb9a81c99
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-21 23:28:30 +0900

    Create P_190.c

---
+++ LangC/Examples/P_190.c
@@ -0,0 +1,20 @@
+//WhenOver5000.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int sum=0, num=0;
+
+	while(1)
+	{
+		sum+=num;
+		if(sum>5000)
+			break;
+		num++;
+	}
+
+	printf("sum: %d\n", sum);
+	printf("num: %d\n", num);
+	return 0;
+}
