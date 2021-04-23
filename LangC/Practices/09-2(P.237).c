22f1e09e20c942d12567e5652bbc84f65f0f1690
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-23 19:06:07 +0900

    Create 09-2(P.237).c

---
+++ LangC/Practices/09-2(P.237).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int AddToTotal(int num)
+{
+	static int total=0;
+	total+=num;
+	return total;
+}
+
+int main(void)
+{
+	int num, i;
+	for(i=0; i<3; i++)
+	{
+		printf("입력%d: ", i+1);
+		scanf("%d", &num);
+		printf("누적: %d\n", AddToTotal(num));
+	}
+	return 0;
+}
