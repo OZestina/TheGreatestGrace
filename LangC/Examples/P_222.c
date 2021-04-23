06d417a934484a799b787f26ba8ba425700811eb
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-23 18:03:17 +0900

    Create P_222.c

---
+++ LangC/Examples/P_222.c
@@ -0,0 +1,28 @@
+//LovalVariable.com
+
+#include <stdio.h>
+
+int SimpleFuncOne(void)
+{
+	int num=10;
+	num++;
+	printf("SimpleFuncOne num: %d \n", num);
+	return 0;
+}
+int SimpleFuncTwo(void)
+{
+	int num1=20;
+	int num2=30;
+	num1++; num2--;
+	printf("num1 & num2: %d %d \n", num1, num2);
+	return 0;
+}
+
+int main (void)
+{
+	int num = 17;
+	SimpleFuncOne();
+	SimpleFuncTwo();
+	printf("main num: %d \n", num);
+	return 0;
+}
