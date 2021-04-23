3073a1bca9c02214dca3d20cd2bfd1697b3f8783
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-23 13:40:13 +0900

    Create P_212.c

---
+++ LangC/Examples/P_212.c
@@ -0,0 +1,18 @@
+//SimpleAddFunc.c
+
+#include <stdio.h>
+
+int Add(int num1, int num2)
+{
+	return num1+num2;
+}
+
+int main(void)
+{
+	int result;
+	result = Add(3,4);
+	printf("덧셈결과1: %d \n", result);
+	result = Add(5,8);
+	printf("덧셈결과2: %d \n", result);
+	return 0;
+}
