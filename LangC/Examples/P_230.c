46cfbb621b23040a5e8fd69d2e0572a251ba6010
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-23 18:28:03 +0900

    Create P_230.c

---
+++ LangC/Examples/P_230.c
@@ -0,0 +1,20 @@
+//GlovalVariable.c
+
+#include <stdio.h>
+void Add(int val);
+int num;
+
+int main(void)
+{
+	printf("num: %d\n", num);
+	Add(3);
+	printf("num: %d\n", num);
+	num++;
+	printf("num: %d\n", num);
+	return 0;
+}
+
+void Add(int val)
+{
+	num+=val;
+}
