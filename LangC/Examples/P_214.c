c6c41a11494f48b621ae909485b7657b73ee4486
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-23 13:55:18 +0900

    Create P_214.c

---
+++ LangC/Examples/P_214.c
@@ -0,0 +1,35 @@
+//SmartAddFunc.c
+
+#include <stdio.h>
+
+int Add(int num1, int num2)
+{
+	return num1+num2;
+}
+void ShowAddResult(int num)
+{
+	printf("%d\n", num);
+}
+int ReadNum(void)
+{
+	int num;
+	scanf("%d", &num);
+	return num;
+}
+void HowToUseThisProg(void)
+{
+	printf("두 개의 정수를 입력하면 덧셈결과가 출력됩니다.\n");
+	printf("자! 그럼 두 개의 정수를 입력하세요.\n");
+}
+
+int main(void)
+{
+	int result, num1, num2;
+	HowToUseThisProg();
+	num1=ReadNum();
+	num2=ReadNum();
+	result = Add(num1,num2);
+	ShowAddResult(result);
+	ShowAddResult(Add(4,5));
+	return 0;
+}
