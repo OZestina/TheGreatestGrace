79aea836613da6b15736b4500cc21d55b2d2572e
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-04-29 23:57:07 +0900

    Create P_266.c

---
+++ LangC/Examples/P_266.c
@@ -0,0 +1,20 @@
+//StartEndString.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str[50]="I like C programming";
+	printf("string: %s\n", str);
+
+	str[8]='\0';
+	printf("string: %s\n", str);
+
+	str[6]='\0';
+	printf("string: %s\n", str);
+
+	str[1]='\0';
+	printf("string: %s\n", str);
+
+	return 0;
+}
