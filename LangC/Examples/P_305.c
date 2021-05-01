48617f13b0c7e96c7b23a7a2ded13dc79c530400
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-02 00:04:26 +0900

    Create P_305.c

---
+++ LangC/Examples/P_305.c
@@ -0,0 +1,13 @@
+//StringArray.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	char* strArr[3]={"Simple", "string", "Array"};
+	printf("%s \n", strArr[0]);
+	printf("%s \n", strArr[1]);
+	printf("%s \n", strArr[2]);
+
+	return 0;
+}
