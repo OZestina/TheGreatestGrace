10e25de6b0a656f9e04bce3c2a54f1f4064e4cf3
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-01 19:07:51 +0900

    Create P_294.c

---
+++ LangC/Examples/P_294.c
@@ -0,0 +1,16 @@
+//ArrayNameIsPointer.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[3]={15, 25, 35};
+	int *ptr=&arr[0]; //int *ptr = arr;
+
+	printf("%d %d\n", ptr[0], arr[0]);
+	printf("%d %d\n", ptr[1], arr[1]);
+	printf("%d %d\n", ptr[2], arr[2]);
+	printf("%d %d\n", *ptr, *arr);
+	
+	return 0;
+}
