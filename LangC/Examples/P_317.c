44b61c22e34642e3cfeaeb877ce797b8252603a6
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-02 00:42:13 +0900

    Create P_317.c

---
+++ LangC/Examples/P_317.c
@@ -0,0 +1,21 @@
+//CallByRefSwap.c
+
+#include <stdio.h>
+
+void Swap(int*ptr1, int*ptr2)
+{
+	int temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
+}
+
+int main(void)
+{
+	int num1=10;
+	int num2=20;
+	printf("num1: %d, num2: %d\n", num1, num2);
+
+	Swap(&num1, &num2);
+	printf("num1: %d, num2: %d\n", num1, num2);
+	return 0;
+}
