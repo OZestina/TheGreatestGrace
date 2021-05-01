eef327d514903ad094e916c1784597eb62b8a711
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-01 19:28:54 +0900

    Create P_297.c

---
+++ LangC/Examples/P_297.c
@@ -0,0 +1,18 @@
+//PointerBaseArrayAccess.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[3] = {11,22,33};
+	int *ptr=arr;
+	printf("%d %d %d \n", *ptr, *(ptr+1), *(ptr+2));
+
+	printf("%d ", *ptr); ptr++;
+	printf("%d ", *ptr); ptr++;
+	printf("%d ", *ptr); ptr--;
+	printf("%d ", *ptr); ptr--;
+	printf("%d ", *ptr); printf("\n");
+	
+	return 0;
+}
