216a6012c86183d3a23102928fe90e3aa47a624e
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-03 23:26:50 +0900

    Create P_344.c

---
+++ LangC/Examples/P_344.c
@@ -0,0 +1,17 @@
+//TwoDimArrayAddr.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[3][2];
+	int i, j;
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<2; j++)
+		{
+			printf("%p\n", &arr[i][j]);
+		}
+	}
+	return 0;
+}
