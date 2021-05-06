c900f9a675ca699558cd4f8868aff351dcffd942
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-07 03:59:53 +0900

    Create P_397.c

---
+++ LangC/Examples/P_397.c
@@ -0,0 +1,21 @@
+//VoidTypePointer.c
+
+#include <stdio.h>
+
+void SoSimpleFunc(void)
+{
+	printf("I'm so simple");
+}
+
+int main(void)
+{
+	int num=20;
+	void*ptr;
+
+	ptr = &num;
+	printf("%p \n", ptr);
+
+	ptr = SoSimpleFunc;
+	printf("%p \n", ptr);
+	return 0;
+}
