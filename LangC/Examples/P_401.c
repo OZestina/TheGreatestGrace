7be91be488ed34ca94a54ab975d7e04446d4f573
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-07 04:25:32 +0900

    Create P_401.c

---
+++ LangC/Examples/P_401.c
@@ -0,0 +1,21 @@
+//ArgcParamType.c
+
+#include <stdio.h>
+
+void ShowAllString(int argc, char*argv[])
+{
+	int i;
+	for (i=0;i<argc; i++)
+		printf("%s \n", argv[i]);
+}
+
+int main(void)
+{
+	char * str[3] = {
+		"C Programming",
+		"C++ Programming",
+		"JAVA Programming"
+	};
+	ShowAllString(3, str);
+	return 0;
+}
