1e52e6068d57200ace662dc6fd41bf9d69d36730
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-08 05:09:52 +0900

    Create P_409.c

---
+++ LangC/Examples/P_409.c
@@ -0,0 +1,16 @@
+//SeedRamdonNum.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int seed, i;
+	printf("씨드 값 입력: ");
+	scanf("%d", &seed);
+	srand(seed);	//사용자는 씨를 뿌렸다. 효과는 굉장했다!
+
+	for(i=0; i<5; i++)
+		printf("정수 출력: %d \n", rand());
+	return 0;
+}
