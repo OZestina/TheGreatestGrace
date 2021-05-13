ae492d472ed164cb3a6b742b5beb6676959992ac
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-13 23:40:21 +0900

    Create P_458.c

---
+++ LangC/Examples/P_458.c
@@ -0,0 +1,25 @@
+//InitStructVal.c
+
+#include <stdio.h>
+
+struct point
+{
+	int xpos;
+	int ypos;
+};
+
+struct person
+{
+	char name[20];
+	char phoneNum[20];
+	int age;
+};
+
+int main(void)
+{
+	struct point pos = {10, 20};
+	struct person man = {"이승기", "010-1122-3344", 21};
+	printf("%d %d \n", pos.xpos, pos.ypos);
+	printf("%s %s %d \n", man.name, man.phoneNum, man.age);
+	return 0;
+}
