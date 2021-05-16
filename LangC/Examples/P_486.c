e6f384842eba5a4d8b3dfadc109a2d4dadcb1cc2
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-17 01:12:17 +0900

    Create P_486.c

---
+++ LangC/Examples/P_486.c
@@ -0,0 +1,32 @@
+//CircleIncludePoint.c
+
+#include <stdio.h>
+
+typedef struct point
+{
+	int xpos;
+	int ypos;
+}Point;
+
+typedef struct circle
+{
+	Point cen;
+	double rad;
+} Circle;
+
+void ShowCircleInfo(Circle * cptr)
+{
+	printf("[%d, %d]\n", (cptr->cen).xpos, (cptr->cen).ypos);
+	printf("%g \n", cptr->rad);
+}
+
+int main(void)
+{
+	Circle c1 = {{1,2}, 3.5};
+	Circle c2 = {{2,4}, 3.9};
+
+	ShowCircleInfo(&c1);
+	ShowCircleInfo(&c2);
+
+	return 0;
+}
