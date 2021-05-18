f0656d8d4c070b36b56df777fc426633a462a7db
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-18 19:52:28 +0900

    Create P_523.c

---
+++ LangC/Examples/P_523.c
@@ -0,0 +1,24 @@
+//ComplexFileWrite.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	char name[10];
+	char sex;
+	int age;
+
+	FILE * fp=fopen("friend.txt", "wt");
+	int i;
+	
+	for(i=0; i<3; i++)
+	{
+		printf("이름 성별 나이 순 입력: ");
+		scanf("%s %c %d", name, &sex, &age);
+		getchar();	//버퍼에 남아있는 \n의 소멸을 위해
+		fprintf(fp, "%s %c %d", name, sex, age);
+	}
+
+	fclose(fp);
+	return 0;
+}
