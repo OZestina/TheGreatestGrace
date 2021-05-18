6f4558f8adf82e11cf11a59d5ab7cbe373893138
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-19 00:11:28 +0900

    Create P_548.c

---
+++ LangC/Examples/P_548.c
@@ -0,0 +1,30 @@
+//ReadStringRight.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char * ReadUserName(void)
+{
+	char * name = (char*)malloc(sizeof(char)*30);
+	printf("What's your name? ");
+	gets(name);
+	return name;
+}
+
+int main(void)
+{
+	char * name1;
+	char * name2;
+	name1 = ReadUserName();
+	printf("name1: %s\n", name1);
+	name2 = ReadUserName();
+	printf("name2: %s\n", name2);
+
+	printf("name1: %s\n", name1);
+	printf("name2: %s\n", name2);
+
+	free(name1);
+	free(name2);
+	
+	return 0;
+}
