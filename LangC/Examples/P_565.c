bebfbf2d3cff9bc14706ebc033fb48e1777e9c41
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-19 02:15:42 +0900

    Create P_565.c

---
+++ LangC/Examples/P_565.c
@@ -0,0 +1,11 @@
+//MacroFunctionWeakness.c
+
+#include <stdio.h>
+#define DIFF_ABS(X,Y)	((X)>(Y)? (X)-(Y): (Y)-(X))
+
+int main(void)
+{
+	printf("두 값의 차: %d \n", DIFF_ABS(5,7));
+	printf("두 값의 차: %g \n", DIFF_ABS(1.8, -1.4));
+	return 0;
+}
