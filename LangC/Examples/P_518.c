c67fc16fece59fa9d303633f8bdbcb000d654d64
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-18 18:54:49 +0900

    Create P_518.c

---
+++ LangC/Examples/P_518.c
@@ -0,0 +1,28 @@
+//TextStringFileCopy.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	FILE * src = fopen("simple.txt", "rt");
+	FILE * des = fopen("des.txt", "wt");
+	char str[20];
+
+	if(src==NULL || des==NULL) {
+		puts("파일오픈 실패!");
+		return -1;
+	}
+
+	while (fgets(str,sizeof(str),src)!=NULL)
+		fputs(str,des);
+
+	if (feof(src)!=0)
+		puts("파일복사 완료");
+	else
+		puts("파일복사 실패");
+	
+	fclose(src);
+	fclose(des);
+
+	return 0;
+}
