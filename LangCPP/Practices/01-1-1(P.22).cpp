bd9d93e68509fff8ee26405f0e21a41867bee7a7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-22 22:06:00 +0900

    Update 01-1-1(P.22).cpp

--- LangCPP/Practices/01-1-1(P.22).cpp
+++ LangCPP/Practices/01-1-1(P.22).cpp
@@ -1,1 +1,38 @@
-//
+#include <iostream>
+
+int main(void)
+{
+	int num[5];
+	int sum=0;
+
+	for(int i=0; i<5; i++)
+	{
+		std::cout<<i+1<<"번째 정수 입력: ";
+		std::cin>>num[i];
+		sum+=num[i];
+	}
+	
+	std::cout<<"합계: "<<sum<<std::endl;
+	
+	return 0;
+}
+
+//굳이 5개의 정수를 따로 입력할 필요는 없어서 (합계만 나오면 되니) 하기와 같이도 가능하다 (메모리를 덜씀)
+#include <iostream>
+
+int main(void)
+{
+	int input;
+	int sum=0;
+
+	for(int i=0; i<5; i++)
+	{
+		std::cout<<i+1<<"번째 정수 입력: ";
+		std::cin>>input;
+		sum+=input;
+	}
+	
+	std::cout<<"합계: "<<sum<<std::endl;
+	
+	return 0;
+}
