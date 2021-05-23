4f9b9e1285d0a67143e1f43f3519ff44f20efc60
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-23 23:21:52 +0900

    Create P_025.cpp

---
+++ LangCPP/Examples/P_025.cpp
@@ -0,0 +1,25 @@
+//FunctionOverloading.cpp
+#include <iostream>
+
+void MyFunc(void)
+{
+	std::cout<<"MyFunc(void) called"<<std::endl;
+}
+
+void MyFunc(char c)
+{
+	std::cout<<"MyFunc(char c) called"<<std::endl;
+}
+
+void MyFunc(int a, int b)
+{
+	std::cout<<"MyFunc(int a, int b) called"<<std::endl;
+}
+
+int main(void)
+{
+	MyFunc();
+	MyFunc('A');
+	MyFunc(3, 4);
+	return 0;
+}
