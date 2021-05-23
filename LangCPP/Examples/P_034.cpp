36130cce8785826b3dc0d5cda2c9bb8c55173f3b
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-23 23:48:31 +0900

    Create P_034.cpp

---
+++ LangCPP/Examples/P_034.cpp
@@ -0,0 +1,15 @@
+//InlineFunc.app
+
+#include <iostream>
+
+inline int SQUARE(int x)
+{
+	return x*x;
+}
+
+int main(void)
+{
+	std::cout<<SQUARE(5)<<std::endl;
+	std::cout<<SQUARE(12)<<std::endl;
+	return 0;
+}
