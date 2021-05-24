54a8e6f63aa37d6fea0e39d20b4a999ec4e1aa92
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-24 16:16:08 +0900

    Create P_038.cpp

---
+++ LangCPP/Examples/P_038.cpp
@@ -0,0 +1,26 @@
+//NameSp1.cpp
+
+#include <iostream>
+
+namespace BestComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout<<"BestCom이 정의한 함수"<<std::endl;
+	}
+}
+
+namespace ProgComImpl
+{
+	void SimpleFunc(void)
+	{
+		std::cout<<"ProgCom이 정의한 함수"<<std::endl;
+	}
+}
+
+int main(void)
+{
+	BestComImpl::SimpleFunc();
+	ProgComImpl::SimpleFunc();
+	return 0;
+}
