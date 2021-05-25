38393a93916a931d469101c4a5d1cb519148ecde
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-25 12:37:39 +0900

    Create P_045.cpp

---
+++ LangCPP/Examples/P_045.cpp
@@ -0,0 +1,19 @@
+//UsingDcl1.cpp
+
+#include <iostream>
+
+namespace Hybrid
+{
+	void HybFunc(void)
+	{
+		std::cout<<"So simple function!"<<std::endl;
+		std::cout<<"In namespace Hybrid!"<<std::endl;
+	}
+}
+
+int main(void)
+{
+	using Hybrid::HybFunc;
+	HybFunc();
+	return 0;
+}
