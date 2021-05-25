713291124daf9d62e40c9c227301ea78853ef90e
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-25 12:41:03 +0900

    Create P_046.cpp

---
+++ LangCPP/Examples/P_046.cpp
@@ -0,0 +1,16 @@
+//UsingDcl2.cpp
+
+#include <iostream>
+using std::cout;
+using std::cin;	//cin 안쓰는데 선언 왜하지..? -> 보여주려구><
+using std::endl;
+
+int main(void)
+{
+	int num=20;
+	cout<<"Hello World!"<<endl;
+	cout<<"Hello "<<"World!"<<endl;
+	cout<<num<<' '<<'A';
+	cout<<' '<<3.14<<endl;
+	return 0;
+}
