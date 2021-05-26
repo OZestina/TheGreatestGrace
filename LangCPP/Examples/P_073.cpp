e4b3c1b397b97534443883d131ab74d33e5cd9de
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-26 23:44:04 +0900

    Create P_073.cpp

---
+++ LangCPP/Examples/P_073.cpp
@@ -0,0 +1,24 @@
+//RefPtr.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int num=12;
+	int *ptr=&num;
+	int **dptr=&ptr;
+
+	int &ref=num;
+	int *(&pref)=ptr;
+	int **(&dpref)=dptr;
+
+	cout<<ref<<endl;
+	cout<<*pref<<endl;
+	cout<<**dpref<<endl;
+
+	cout<<pref<<endl;
+	cout<<*dpref<<endl;
+
+	return 0;
+}
