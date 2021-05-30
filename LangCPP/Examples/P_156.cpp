31a6ec025852ebe2bfafb6c5f7eee0cd222d7357
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-05-31 00:00:17 +0900

    Create P_156.cpp

---
+++ LangCPP/Examples/P_156.cpp
@@ -0,0 +1,19 @@
+//Rectangle.h
+
+#ifndef __RECTANGLE_H_
+#define __RECTANGLE_H_
+
+#include "Point.h"
+
+class Rectangle
+{
+private:
+	Point upLeft;
+	Point lowRight;
+
+public:
+	bool InitMembers(const Point &ul, const Point &lr);
+	void ShowRecInfo() const;
+};
+
+#endif
