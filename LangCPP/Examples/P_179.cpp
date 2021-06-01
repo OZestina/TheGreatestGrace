514e3ca6618915fb7cf18c69bbdf198905180545
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-01 23:55:16 +0900

    Create P_179.cpp

---
+++ LangCPP/Examples/P_179.cpp
@@ -0,0 +1,13 @@
+//RectangleConstructor.cpp
+
+#include <iostream>
+#include "Point.h"
+#include "Rectangle.h"
+using namespace std;
+
+int main(void)
+{
+	Rectangle rec(1,1,5,5);
+	rec.ShowRecInfo();
+	return 0;
+}
