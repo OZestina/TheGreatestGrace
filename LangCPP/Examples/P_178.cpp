915e2f493249378e1f07cb3c7061385f43979dbc
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-01 23:54:41 +0900

    Create P_178.cpp

---
+++ LangCPP/Examples/P_178.cpp
@@ -0,0 +1,19 @@
+//Rectangle.h
+
+#ifndef __RECTANGLE_H_
+#define __RECTANGLE_H_
+
+#include "Point.h"
+
+class Rectangle
+{
+private:
+	Point upLeft;
+	Point lowRight;
+
+public:
+	Rectangle(const int &x1, const int &y1, const int &x2, const int &y2);
+	void ShowRecInfo() const;
+};
+
+#endif
