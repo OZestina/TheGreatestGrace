be9e4b1593c29a895efa598773d20050b46f049c
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-01 23:48:31 +0900

    Create P_177.cpp

---
+++ LangCPP/Examples/P_177.cpp
@@ -0,0 +1,20 @@
+//Point.h
+
+#ifndef __POINT_H_
+#define __POINT_H_
+
+class Point
+{
+private:
+	int x;
+	int y;
+
+public:
+	Point(const int &xpos, const int &ypos);
+	int GetX() const;
+	int GetY() const;
+	bool SetX(int xpos);
+	bool SetY(int ypos);
+};
+
+#endif
