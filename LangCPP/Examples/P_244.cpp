aabf029e6764133220c12af6de0da113288520f7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-05 19:52:27 +0900

    Create P_244.cpp

---
+++ LangCPP/Examples/P_244.cpp
@@ -0,0 +1,30 @@
+//ConstObject.cpp
+
+#include <iostream>
+using namespace std;
+
+class SoSimple
+{
+private:
+	int num;
+public:
+	SoSimple(int n): num(n)
+	{}
+	SoSimple& AddNum(int n)
+	{
+		num += n;
+		return *this;
+	}
+	void ShowData() const
+	{
+		cout<<"num: "<<num<<endl;
+	}
+};
+
+int main(void)
+{
+	const SoSimple obj(7);
+	//obj.AddNum(20);
+	obj.ShowData();
+	return 0;
+}
