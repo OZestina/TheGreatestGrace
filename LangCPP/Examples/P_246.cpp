bb2741ffa858b362911867f0efebe9b735b7f4f6
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-05 20:12:26 +0900

    Create P_246.cpp

---
+++ LangCPP/Examples/P_246.cpp
@@ -0,0 +1,48 @@
+//ConstOverloading.cpp
+
+#include <iostream>
+using namespace std;
+
+class SoSimple
+{
+private:
+	int num;
+public:
+	SoSimple(int n): num(n)
+	{}
+	SoSimple& AddNum(int n)
+	{
+		num += n;
+		return *this;
+	}
+	void SimpleFunc()
+	{
+		cout<<"SimpleFunc: "<<num<<endl;
+	}
+	void SimpleFunc() const
+	{
+		cout<<"const SimpleFunc: "<<num<<endl;
+	}
+	void ShowData() const
+	{
+		cout<<"num: "<<num<<endl;
+	}
+};
+
+void YourFunc(const SoSimple &obj)
+{
+	obj.SimpleFunc();
+}
+
+int main(void)
+{
+	SoSimple obj1(2);
+	const SoSimple obj2(7);
+
+	obj1.SimpleFunc();
+	obj2.SimpleFunc();
+
+	YourFunc(obj1);
+	YourFunc(obj2);
+	return 0;
+}
