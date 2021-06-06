9bc2d3f3ed32d832dd7cb1f902999ce638010115
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-06 16:41:52 +0900

    Create P_256.cpp

---
+++ LangCPP/Examples/P_256.cpp
@@ -0,0 +1,44 @@
+//NeedGlobal.cpp
+
+#include <iostream>
+using namespace std;
+
+int simObjCnt = 0;
+int cmxObjCnt = 0;
+
+class SoSimple
+{
+public:
+	SoSimple()
+	{
+		simObjCnt++;
+		cout<<simObjCnt<<"번째 SoSimple 객체"<<endl;
+	}
+};
+
+class SoComplex
+{
+public:
+	SoComplex()
+	{
+		cmxObjCnt++;
+		cout<<cmxObjCnt<<"번째 So Complex 객체"<<endl;
+	}
+	SoComplex(SoComplex &copy)
+	{
+		cmxObjCnt++;
+		cout<<cmxObjCnt<<"번째 So Complex 객체"<<endl;
+	}
+};
+
+int main(void)
+{
+	SoSimple sim1;
+	SoSimple sim2;
+
+	SoComplex com1;
+	SoComplex com2=com1;
+	SoComplex();
+
+	return 0;
+}
