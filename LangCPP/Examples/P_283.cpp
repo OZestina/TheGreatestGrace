7221b141c8d9f88069923c7db5195136117dda55
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-07 13:34:02 +0900

    Create P_283.cpp

---
+++ LangCPP/Examples/P_283.cpp
@@ -0,0 +1,52 @@
+//UnivStudentInheri.cpp
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person
+{
+private:
+	int age;
+	char name[50];
+public:
+	Person(int myage, char * myname): age(myage)
+	{
+		strcpy(name, myname);
+	}
+	void WhatYourName() const
+	{
+		cout<<"My name is "<<name<<endl;
+	}
+	void HowOldAreYou() const
+	{
+		cout<<"I'm "<<age<<" years old"<<endl;
+	}
+};
+
+class UnivStudent : public Person
+{
+private:
+	char major[50];
+public:
+	UnivStudent(char* myname, int myage, char* mymajor): Person(myage, myname)
+	{
+		strcpy(major, mymajor);
+	}
+	void WhoAreYou() const
+	{
+		WhatYourName();
+		HowOldAreYou();
+		cout<<"My major is "<<major<<endl<<endl;
+	}
+};
+
+int main(void)
+{
+	UnivStudent ustd1("Lee", 22, "Computer Eng.");
+	ustd1.WhoAreYou();
+
+	UnivStudent ustd2("Yoon", 21, "Business");
+	ustd2.WhoAreYou();
+	return 0;
+}
