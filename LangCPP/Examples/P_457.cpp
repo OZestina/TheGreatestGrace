67ba22a910e889783d449d8457f630d286b94432
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-11 18:50:44 +0900

    Create P_457.cpp

---
+++ LangCPP/Examples/P_457.cpp
@@ -0,0 +1,40 @@
+//ArrayClass.cpp
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class BoundCheckIntArray
+{
+private:
+	int * arr;
+	int arrlen;
+public:
+	BoundCheckIntArray(int len): arrlen(len)
+	{
+		arr = new int[len];
+	}
+	int& operator[] (int idx)
+	{
+		if(idx<0 || idx>=arrlen)
+		{
+			cout<<"Array index out of bound exceptation"<<endl;
+			exit(1);
+		}
+		return arr[idx];
+	}
+	~BoundCheckIntArray()
+	{
+		delete []arr;
+	}
+};
+
+int main(void)
+{
+	BoundCheckIntArray arr(5);
+	for(int i=0; i<5; i++)
+		arr[i] = (i+1)*11;
+	for(int i=0; i<5; i++)
+		cout<<arr[i]<<endl;
+	return 0;
+}
