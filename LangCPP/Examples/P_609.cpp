4027ab948c1332762dc5bed92e54646c0ec5df53
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-15 08:16:00 +0900

    Create P_609.cpp

---
+++ LangCPP/Examples/P_609.cpp
@@ -0,0 +1,101 @@
+//ATMSim.cpp
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class AccountException
+{
+public:
+	virtual void ShowExceptionReason()=0;	//순수 가상함수
+};
+
+class DepositException: public AccountException
+{
+private:
+	int reqDep;	//요청 입금액
+public:
+	DepositException(int money): reqDep(money) { }
+	void ShowExceptionReason()
+	{
+		cout<<"[예외 메시지: "<<reqDep<<"는 입금불가]"<<endl;
+	}
+};
+
+class WithdrawException: public AccountException
+{
+private:
+	int balance; //잔고
+public:
+	WithdrawException(int money): balance(money) { }
+	void ShowExceptionReason()
+	{
+		cout<<"[예외 메시지: 잔액 "<<balance<<", 잔액부족]"<<endl;
+	}
+};
+
+class Account
+{
+private:
+	char accNum[50];	//계좌번호
+	int balance;		//잔고
+public:
+	Account(char* acc, int money): balance(money)
+	{
+		strcpy(accNum, acc);
+	}
+	void Deposit(int money) throw (AccountException)
+	{
+		if(money<0)
+		{
+			DepositException expn(money);
+			throw expn;
+		}
+		balance += money;
+	}
+	void Withdraw(int money) throw (AccountException)
+	{
+		if(balance<money)
+		{
+			WithdrawException expn(balance);
+			throw expn;
+		}
+		balance -= money;
+	}
+	void ShowMyMoney()
+	{
+		cout<<"잔고: "<<balance<<endl<<endl;
+	}
+};
+
+int main(void)
+{
+	Account myAcc("56789-123456", 5000);
+	myAcc.ShowMyMoney();
+
+	try
+	{
+		myAcc.Deposit(2000);
+		myAcc.ShowMyMoney();
+		myAcc.Deposit(-300);
+	}
+	catch(AccountException &expn)
+	{
+		expn.ShowExceptionReason();
+	}
+	
+	try
+	{
+		myAcc.Withdraw(3500);
+		myAcc.ShowMyMoney();
+		myAcc.Withdraw(4500);
+	}
+	catch(AccountException &expn)
+	{
+		expn.ShowExceptionReason();
+	}
+
+	myAcc.ShowMyMoney();
+	
+	return 0;
+}
