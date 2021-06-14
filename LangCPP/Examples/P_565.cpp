717fd5c5ef8264eac35d956e211ce3ab283a59ab
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-15 04:59:43 +0900

    Create P_565.cpp

---
+++ LangCPP/Examples/P_565.cpp
@@ -0,0 +1,47 @@
+//PointTemplateFriendFunction.cpp
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Point
+{
+private:
+	T xpos, ypos;
+public:
+	Point(T x=0, T y=0);
+	void ShowPosition() const;
+
+	friend Point<int> operator+(const Point<int>&, const Point<int>&);
+	friend ostream& operator<<(ostream& os, const Point<int>& pos);
+};
+
+template <typename T>
+Point<T>::Point(T x, T y): xpos(x), ypos(y) {}
+
+template <typename T>
+void Point<T>::ShowPosition() const
+{
+	cout<<'['<<xpos<<", "<<ypos<<']'<<endl;
+}
+
+Point<int> operator+(const Point<int>& pos1, const Point<int>& pos2)
+{
+	return Point<int> (pos1.xpos+pos2.xpos, pos1.ypos+pos2.ypos);
+}
+
+ostream& operator<<(ostream& os, const Point<int>& pos)
+{
+	os<<'['<<pos.xpos<<", "<<pos.ypos<<']'<<endl;
+	return os;
+}
+
+
+int main(void)
+{
+	Point<int> pos1(2,4);
+	Point<int> pos2(4,8);
+	Point<int> pos3 = pos1+pos2;
+	cout<<pos1<<pos2<<pos3;
+	return 0;
+}
