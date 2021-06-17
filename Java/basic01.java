639025dae8c2b39fda13739ae5741cbd76886593
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-17 21:21:57 +0900

    Create basic01.java

---
+++ Java/basic01.java
@@ -0,0 +1,16 @@
+public class Datatype {
+  public static void main(String[] args) {
+    System.out.println(6);  //Number
+    System.out.println("Six");  //String
+    System.out.println("6");  //String 6
+    
+    System.out.println(6+6);  //12
+    System.out.println("6"+"6");  //66
+    
+    System.out.println(6*6);  //36
+    //System.out.println("6"*"6");  //error
+    
+    System.out.println("1111".length());  //4
+    //System.out.println(1111.length());  //length()는 문자열 길이 구하는 함수, 숫자에 사용할 수 없다
+  }
+}
