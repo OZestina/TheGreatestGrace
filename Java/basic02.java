8e9b5d92c1636f1b9c3e08a302e992f4b880d6c4
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-17 21:50:26 +0900

    Create basic02.java

---
+++ Java/basic02.java
@@ -0,0 +1,18 @@
+public class Number {
+  
+  public static void main(String[] args) {
+    //Operator
+    System.out.println(6+2);  //8
+    System.out.println(6-2);  //4
+    System.out.println(6*2);  //12
+    System.out.println(6/2);  //3
+    
+    System.out.println(Math.PI);  //3.141592...
+    System.out.println(Math.floor(Math.PI));  //3.0 //floor 내림
+    System.out.println(Math.ceil(Math.PI));  //4.0  //ceil 올림
+    System.out.println(Math.round(Math.PI));  //4.0  //round 반올림
+    System.out.println(Math.abs(Math.PI));  //4.0  //abs 절대값
+    System.out.println(Math.pow(4));  //16  //pow power 제곱
+    System.out.println(Math.sqrt(4));  //2  //sqrt square root 제곱근
+  }
+}
