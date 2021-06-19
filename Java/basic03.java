ccc7611c8e5aa64bf08117a508ee21a663670bbc
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-19 21:36:08 +0900

    Update basic03.java

--- Java/basic03.java
+++ Java/basic03.java
@@ -1,1 +1,17 @@
-//to be continued...
+public class StringApp {
+  
+  public static void main(String[] args) {
+    
+    System.out.println("Hello World");  //String
+    System.out.println("H");            //String
+    System.out.println('H');            //Character
+    
+    System.out.println("Hello "
+                       + "World");      //+ 는 문자열을 더해주는(이어지게 하는) 것
+    
+    System.out.println("Hello \nWorld");  //\n 은 줄바꿈 문자
+    
+    System.out.println("Hello \"World\"");  // Hello "World"
+    
+  }
+}
