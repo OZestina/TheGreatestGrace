709bcb52153e22908a20c55139fb0ccc1b4a88fd
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-28 00:16:29 +0900

    Create P_046.java

---
+++ Java/Examples/P_046.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args){
+
+        int number = 10;
+        long number2 = number;  //자동형변환
+
+        double pi = 3.14;
+        int pi2 = (int)pi;  //강제형변환
+        System.out.println(pi2);    //3
+    }
+}
