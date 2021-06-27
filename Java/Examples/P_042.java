5eb608604fc97a697201d135eb749736a78fdf0c
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-27 23:52:56 +0900

    Create P_042.java

---
+++ Java/Examples/P_042.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args){
+
+        String name = "홍길동";
+        String name1 = null;
+        String name2 = "";
+
+        System.out.println("name: "+ name);
+        System.out.println("name1: "+name1);
+        System.out.println("name2: "+name2);
+
+    }
+}
