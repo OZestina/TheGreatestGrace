1fc93d66da724b16a9ace8a261ea460206f1bbc7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-28 00:28:42 +0900

    Create P_053.java

---
+++ Java/Examples/P_053.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        System.out.println("number1 = 10 -> " + number1);
+        number1 += 10;
+        System.out.println("number1 += 10 -> " + number1);
+        number1 -= 10;
+        System.out.println("number1 -= 10 -> " + number1);
+        number1 *= 2;
+        System.out.println("number1 *= 2 -> " + number1);
+        number1 /= 2;
+        System.out.println("number1 /= 2 -> " + number1);
+        number1 %= 10;
+        System.out.println("number1 %= 3 -> " + number1);
+
+    }
+}
