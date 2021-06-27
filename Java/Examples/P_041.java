b064c3799bd3c5dfeb9e8f70ce933cd204819224
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-27 23:43:32 +0900

    Create P_041.java

---
+++ Java/Examples/P_041.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args){
+
+        float f = 3.141592653589793f;
+        double d = 3.141592653589793d;
+
+        System.out.println("float: "+f);
+        System.out.println("double: "+d);
+
+        double d2 = 3.141592653589793;
+        System.out.println("double: "+d);
+
+    }
+}
