5d428a194fa7ecdddf1d8b4fa46b6d9e9b6882b7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-28 00:31:51 +0900

    Create P_054.java

---
+++ Java/Examples/P_054.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int a = 5+5;
+        System.out.println(a);  //10
+        int b = a-5;
+        System.out.println(b);  //5
+        int c = b*2;
+        System.out.println(c);  //10
+        int d = c/5;
+        System.out.println(d);  //2
+
+        int e = 10%3;
+        System.out.println(e);  //1
+
+    }
+}
