187ca59aa93f8fe055612731ee2f3c222dbc2ec5
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-28 01:06:03 +0900

    Create P_056.java

---
+++ Java/Examples/P_056.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        System.out.println("+a = "+ +a);
+        System.out.println("-a = "+ -a);
+
+        a = -5;
+        System.out.println("+a = "+ +a);    //부호를 '지정'하는 연산자
+        System.out.println("-a = "+ -a);    //부호를 '지정'하는 연산자
+
+    }
+}
