dc9b6e56bbdda0d8015b82aae06d213469ecc380
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-28 00:10:16 +0900

    Create P_045.java

---
+++ Java/Examples/P_045.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Practice {
+
+    int no;
+
+    public static void main(String[] args){
+
+        String name;
+
+        if (true) {
+
+            name = "홍/t길동";
+            String email = "hong@test.co.kr";
+        }
+
+        //System.out.println(email);    //중괄호 안에서 선언된 변수는 중괄호 안에서만 사용 가능하다
+    }
+}
