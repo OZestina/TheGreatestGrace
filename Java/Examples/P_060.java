0f661dc6814c18da265c1ed05bc87fb703227550
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-06-29 14:20:00 +0900

    Create P_060.java

---
+++ Java/Examples/P_060.java
@@ -0,0 +1,21 @@
+//boolean 자료형 변수에도 이렇게 대입이 가능함!
+//이거 재미있네
+
+
+package 복습;
+
+public class practice {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 10;
+		
+		boolean c = a==b;
+		boolean d = c==false;
+		System.out.println("c: "+c);
+		System.out.println("d: "+d);
+		
+	}
+
+}
