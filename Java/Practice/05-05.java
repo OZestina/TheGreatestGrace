3587a92443c82545f1c36976dfc5ed77020fd509
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-07-01 18:10:07 +0900

    Create 05-05.java

---
+++ Java/Practice/05-05.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}// main
+}
