61dde12dc3b4d9db4bd59080890b6822c27f3085
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-07-05 16:36:31 +0900

    Create P_064.java

---
+++ Java/Examples/P_064.java
@@ -0,0 +1,17 @@
+package 문제풀이;
+
+public class Practice {
+	
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 0;
+		
+		//&연산
+		System.out.println(b>0 & (a/b>0));	//뒷 내용 때문에 에러뜸! 
+    
+    //&&연산이었으면 뒷 내용 연산 필요가 없어서 스무스하게 넘어갈 수 있다네~
+
+	}
+
+}
