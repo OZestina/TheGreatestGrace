e22b30fc86d960d4c54a1d382a8259c501d5bc31
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-07-07 01:30:11 +0900

    Create P_069.java

---
+++ Java/Examples/P_069.java
@@ -0,0 +1,14 @@
+package practice;
+
+import java.util.Random;
+
+public class Practice {
+
+    public static void main(String[] args) {
+
+        int score = 80;
+        String pass = score>= 60? "합격" : "불합격";
+        System.out.println(pass);
+
+    }
+}
