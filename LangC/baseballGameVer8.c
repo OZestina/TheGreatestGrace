8ef9c48e2c35bd0d9fb60c2f74ae2ebebcfd417a
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-07-25 01:09:47 +0900

    Create baseballGameVer8.c

---
+++ LangC/baseballGameVer8.c
@@ -0,0 +1,17 @@
+//updated 21.nn.nn
+
+/* Assignment
+1) gotoXY 함수로 GameScreen 클래스를 읽기 쉽게 만들어보자
+
+GameScreen {
+  printFrame();
+  printPrevInput();
+  printStatus();
+}
+void gotoxy(int x, int y);  //<Windows.h> 헤더 추가 필요
+  : 콘솔 내 문자 좌표 커서를 x,y로 이동시키는함수
+  
+	> 프레임을 그린다
+  > (커서이동) 플레이어가 입력한 배열 및 SBO 출력
+  > (커서이동) 플레이 결과문 출력하는 위치로 커서 이동
+*/
