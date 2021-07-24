327d33c8c23ee518e5fd147fb8fee60346de20f7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-07-24 22:29:04 +0900

    Update baseballGameVer7.c

--- LangC/baseballGameVer7.c
+++ LangC/baseballGameVer7.c
@@ -2,7 +2,9 @@
 
 /* Assignment
 1) GameScreen 함수 선언으로 Game 함수 간략화
-    - Game 함수 내 GameScreen 함수 
+	- Game 함수 내 GameScreen 함수
+2) 화면꾸미는 문자를 상수로 설정! 하나만 바꾸면 취향에 맞게 수정 가능!
+	- line 14
 */
 
 #include <stdlib.h>
@@ -10,6 +12,7 @@
 #include <time.h>
 
 #define GAME_OPTION 9
+#define icon "#"
 
 int nums[GAME_OPTION];	//컴퓨터 난수 배열 저장
 int arr[GAME_OPTION];	//게이머 배열 저장
@@ -56,9 +59,57 @@ void NumMake(int nums[], int list[], int n)
 
 }
 
+void GameScreen(int arr[], int strike, int ball, int outt, int n)
+{
+	int i;
+
+	system("cls");                  //화면 정리용
+	for (i = 0; i < 36; i++) { printf("%s", icon); } printf("\n");
+	printf("%s                                  %s\n", icon, icon);
+
+	//이전 단계 입력 수 보여주긔
+	printf("#  Number: ");
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	for (i = 0; i < 9 - n; i++)
+	{
+		printf("  ");
+	}
+	printf("      #\n");
+	printf("%s                                  %s\n", icon, icon);
+	printf("%s ================================ %s\n", icon, icon);
+	printf("%s                                  %s\n", icon, icon);
+
+	//strike
+	printf("%s  S : ", icon);
+	for (i = 0; i < strike; i++) { printf("●"); }
+	for (i = 0; i < (n - strike); i++) { printf("○"); }
+	for (i = 0; i < 9 - n; i++) { printf("  "); }
+	printf("          %s\n",icon);
+
+	//ball
+	printf("%s  B : ", icon);
+	for (i = 0; i < ball; i++) { printf("●"); }
+	for (i = 0; i < (n - ball); i++) { printf("○"); }
+	for (i = 0; i < 9 - n; i++) { printf("  "); }
+	printf("          %s\n", icon);
+
+	//out
+	printf("%s  O : ", icon);
+	for (i = 0; i < outt; i++) { printf("●"); }
+	for (i = 0; i < (n - outt); i++) { printf("○"); }
+	for (i = 0; i < 9 - n; i++) { printf("  "); }
+	printf("          %s\n", icon);
+	printf("%s                                  %s\n", icon, icon);
+	for (i = 0; i < 36; i++) { printf("%s", icon); } printf("\n");
+}
+
 void Game(int arr[], int nums[], int list[], int n)
 {
-	int strike = 0, ball=0, outt=0;
+	int strike = 0, ball = 0, outt = 0;
 	int num = 0;	//n번째 시도 카운팅
 	int i;
 
@@ -78,7 +129,7 @@ void Game(int arr[], int nums[], int list[], int n)
 			GameScreen(arr, strike, ball, outt, n);
 			printf("다시 시도해보세요. %d번째 시도입니다. \n", num + 1);
 		}
-			
+
 		for (i = 0; i < n; i++)
 		{
 			scanf_s("%d", &arr[i]);
@@ -106,54 +157,6 @@ void Game(int arr[], int nums[], int list[], int n)
 	//게임 승리 시
 	GameScreen(arr, strike, ball, outt, n);
 	printf("%d 스트라이크! 축하합니다. \n%d번 만에 성공했어요.\n\n", n, num);
-}
-
-void GameScreen(int arr[], int strike, int ball, int outt, int n)
-{
-	int i;
-
-  system("cls");                  //화면 정리용
-	printf("####################################\n");
-	printf("#                                  #\n");
-
-	//이전 단계 입력 수 보여주긔
-	printf("#  Number: ");
-	for (i = 0; i < n; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-
-	for (i = 0; i < 9 - n; i++)
-	{
-		printf("  ");
-	}
-	printf("      #\n");
-	printf("#                                  #\n");
-	printf("# ================================ #\n");
-	printf("#                                  #\n");
-
-	//strike
-	printf("#  S : ");
-	for (i = 0; i < strike; i++) { printf("●"); }
-	for (i = 0; i < (n - strike); i++) { printf("○"); }
-	for (i = 0; i < 9 - n; i++) { printf("  "); }
-	printf("          #\n");
-
-	//ball
-	printf("#  B : ");
-	for (i = 0; i < ball; i++) { printf("●"); }
-	for (i = 0; i < (n - ball); i++) { printf("○"); }
-	for (i = 0; i < 9 - n; i++) { printf("  ");	}
-	printf("          #\n");
-
-	//out
-	printf("#  O : ");
-	for (i = 0; i < outt; i++) { printf("●"); }
-	for (i = 0; i < (n - outt); i++) { printf("○"); }
-	for (i = 0; i < 9 - n; i++) { printf("  "); }
-	printf("          #\n");
-	printf("#                                  #\n");
-	printf("####################################\n");
 }
 
 int NewGame(void)
