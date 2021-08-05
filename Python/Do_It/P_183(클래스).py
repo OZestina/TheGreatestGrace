0579c8bffeadcc44b299ce3678552cc6ccab51cb
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-08-05 14:17:41 +0900

    Update P_183(클래스).py

--- Python/Do_It/P_183(클래스).py
+++ Python/Do_It/P_183(클래스).py
@@ -11,14 +11,38 @@ class Calculator:
         return self.result
 
 class FourCal:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
     def setdata(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
     def add(self):
-        result = self.num1 + self.num2
+        return self.num1 + self.num2
     def sub(self):
-        result = self.num1 - self.num2
+        return self.num1 - self.num2
     def mul(self):
-        result = self.num1 * self.num2
+        return self.num1 * self.num2
     def div(self):
-        result = self.num1 / self.num2
+        return self.num1 / self.num2
+
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.num1 ** self.num2
+        return result
+
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.num2 == 0:
+            return 0
+        else:
+            return self.num1 / self.num2
+
+class Family:
+    lastname = 'kim'
+
+print(Family.lastname)
+a = Family()
+
+print(id(a.lastname))
+print(id(Family.lastname))
