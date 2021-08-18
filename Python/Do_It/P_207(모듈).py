8ae09064d1a2002377c70a93793fbb70292e4191
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-08-19 02:44:18 +0900

    Update P_207(모듈).py

--- Python/Do_It/P_207(모듈).py
+++ Python/Do_It/P_207(모듈).py
@@ -1,5 +1,10 @@
+#mod1.py를 import
 import mod1
 
+#from mod1 import add => add함수 모듈명없이 add로 바로 사용 가능
+#from mod1 import *   => mod1에 속한 함수 모듈명없이 함수명으로 바로 사용 가능
+
+#import한 모듈의 함수를 쓸 때는 모듈.함수명()으로 사용
 a = mod1.add(3,4)
 print(a)
 
@@ -13,13 +18,16 @@ class Math:
     def solv(self, r):
         return PI * (r ** 2)
 
-
 def add(a, b):
     return a + b
+
+#__name__ : 내부적으로 사용하는 특별한 변수로, 
+#직접 실행하면 __main__으로,
+#import돼 실행되면 (모듈로 실행되면) 모듈명(이 경우에는 mod2)으로 저장됨
+if __name__ == "__main__":
+    print("mod2.py를 실행합니다")
 #-------------------------------------------
 
-# calculator 모듈에 있는 모든 것(*)을 import 하겠다
-# 이렇게 하면 calculator.함수명 대신 함수를 바로 사용할 수 있다
 from calculator import *
 import mod2
 
@@ -30,6 +38,7 @@ a = mod2.Math()
 print(a.solv(2))
 
 #왜 아래에는 solv에 self값도 넣어줘야하지..?
+#self값은 객체명이 들어가는 곳인데, 객체 생성없이 바로 써서 그런가보다^^
 print(mod2.Math.solv(0,2))
 
 print(mod2.add(3,4))
