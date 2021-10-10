009a89933ca66e209d0d03a1174bfc5ddb1d23bd
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-10-10 23:44:16 +0900

    Update Practices(P.179).py

--- Python/Do_It/Practices(P.179).py
+++ Python/Do_It/Practices(P.179).py
@@ -1,3 +1,4 @@
+#Q1
 def is_odd(number):
     if number%2==1:
         return True
@@ -6,22 +7,26 @@ def is_odd(number):
 
 is_even = lambda x : True if x%2==0 else False
 
+#Q2
 def avg_numbers(*args):
     result = 0
     for i in args:
         result+=i
     return result/len(args)
 
+#Q3
 input1 = input("enter 1st number")
 input2 = input("enter 2nd number")
 total = int(input1) + int(input2)
 print("sum of two numbers is %s" %total)
 
+#Q5
 with open("test.txt", "w") as f:
     f.write("Life is too short")
 with open("test.txt", "r") as f1:
     print(f1.read())
 
+#Q6
 user_input = input("write what you want to keep>> ")
 with open("test.txt","a") as f:
     f.write(user_input)
@@ -29,6 +34,7 @@ with open("test.txt","a") as f:
 with open("test.txt","r") as f:
     print(f.read())
 
+#Q7
 with open('test.txt', 'r') as f:
     body = f.read()
 body = body.replace("java","python")
