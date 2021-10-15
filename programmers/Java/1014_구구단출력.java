6cf48bcecf6ea8a4b9e2862555d20047dc5b216f
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-10-15 20:37:33 +0900

    Update 1014_구구단출력.java

--- programmers/Java/1014_구구단출력.java
+++ programmers/Java/1014_구구단출력.java
@@ -10,3 +10,17 @@ public void ggd() {
     System.out.println();
   }
 }
+
+
+
+//구구단 3단에서 4의 배수를 제외하고 출력
+public void ggd3() {
+  for (int i = 2; i < 10; i++) {
+    System.out.println("=====[ "+i+"단 ]=====");
+    for (int j = 1; j < 10; j++) {
+      if (i==3 & j%4 == 0) { continue; }
+      else { System.out.println(i+" x "+j+" = "+(i*j)); }
+    }
+    System.out.println();
+  }
+}
