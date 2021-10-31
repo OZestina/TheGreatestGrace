efb3839f2bd8f63bfca7e290fa8c2ce5c7251e1f
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-10-31 20:23:34 +0900

    Create 1031_음양더하기.py

---
+++ codingTest/programmers/py/1031_음양더하기.py
@@ -0,0 +1,10 @@
+#https://programmers.co.kr/learn/courses/30/lessons/76501
+
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
+    return answer
