093083a5a9980c843c83dc46a3c3bb96fbde13e7
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-03 18:24:04 +0900

    Update 1103_정사각형.java

--- codingTest/goorm/Java/1103_정사각형.java
+++ codingTest/goorm/Java/1103_정사각형.java
@@ -14,5 +14,22 @@ class Main {
 		}
 		System.out.println(result);
 	}
-	
+}
+
+
+//i*i말고, i의 n제곱근을 구하는 Math.pow(i, n)으로 풀어본 것
+
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int square = Integer.valueOf(input);
+		
+		long result = 0L;
+		for (int i = 1; i < square+1; i++) {
+			result += (long)Math.pow(i, 2);
+		}
+		System.out.println(result);
+	}
 }
