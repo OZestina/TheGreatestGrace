802aaa39a2ff2cc6fb35777a5095a859d9126247
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-19 00:04:25 +0900

    Update 1118_뿌요.py

--- codingTest/goorm/py/1118_뿌요.py
+++ codingTest/goorm/py/1118_뿌요.py
@@ -9,36 +9,32 @@ result = input()
 #각 알파벳의 첫 시작 위치 저장
 idx = [0]
 #반복되는 알파벳의 가장 마지막 수 저장
-r_idx = 1
+pos = 1
 
-while r_idx < len(result):
+while pos < len(result):
 	#r_idx가 result 길이보다 작을때 반복
 	if len(idx) == 0:
-		idx.append(r_idx)
+		idx.append(pos)
 	#idx에 마지막으로 저장된 알파벳과 현재 알파벳이 같을 경우 r_idx+1
-	while r_idx < len(result) and result[r_idx] == result[len(idx)-1]:
-		r_idx += 1
+	while pos < len(result) and result[pos] == result[idx[len(idx)-1]]:
+		pos += 1
+		# print(pos)
 	#반복된 알파벳의 개수가 M 이상이면 해당 부분 삭제
-	if r_idx - idx[len(idx)-1] >= M:
-		result = result.replace(result[len(idx)-1]*(r_idx - idx[len(idx)-1]),'',1)
-		r_idx = idx[len(idx)-1]
+	if pos - idx[len(idx)-1] >= M:
+		result = result.replace(result[idx[len(idx)-1]]*(pos - idx[len(idx)-1]),'',1)
+		pos = idx[len(idx)-1]
 		del idx[len(idx)-1]
-		# print(result)
-		# print("if: ", idx)
 	#반복된 알파벳의 개수가 M 미만이면 r_idx를 idx배열에 추가
 	else:
-		idx.append(r_idx)
-		r_idx += 1
-		# print("else: ", idx)
-if len(idx) == 0:
-	print("CLEAR!")
-else:
-	print(result)
+		idx.append(pos)
+		pos += 1
 
+if len(idx) == 0: print("CLEAR!")
+else: print(result)
 	
 	
 #2nd try
-#배열을 포기하고, 스트링 삭제 후 처음부터 진행하는 방법으로 변경
+#배열을 사용하지 않고, 스트링 삭제 후 처음부터 진행하는 방법으로 변경
 #첫 번째 방법이 연산횟수가 더 적은데, 런타임에러와 에러가 뜨는 이유는 뭘까...
 
 [N,M] = [int(x) for x in input().split(" ")]
@@ -50,7 +46,7 @@ idx1 = 0
 idx2 = 1
 
 while idx2 < len(result):
-	#idx에 마지막으로 저장된 알파벳과 현재 알파벳이 같을 경우 r_idx+1
+	#idx에 마지막으로 저장된 알파벳과 현재 알파벳이 같을 경우 idx2+1
 	while idx2 < len(result) and result[idx2] == result[idx1]:
 		idx2 += 1
 	#반복된 알파벳의 개수가 M 이상이면 해당 부분 삭제
