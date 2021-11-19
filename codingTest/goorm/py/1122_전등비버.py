6ebf16b8ca1134bb1d1a4f39ea62f75fe01e7a11
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-19 18:08:46 +0900

    Create 1122_전등비버.py

---
+++ codingTest/goorm/py/1122_전등비버.py
@@ -0,0 +1,21 @@
+#https://level.goorm.io/exam/51584/%EA%B5%AC%EB%A6%84-%EA%B2%8C%EC%9E%841-%EB%AF%B8%EC%99%84%EC%84%B1/quiz/1
+
+[N,M] = [int(x) for x in input().split(" ")]
+lamp = []
+for i in range(N):
+	lamp.append([int(x) for x in input().split(" ")])
+	
+times = int(input())
+for i in range(times):
+	[direction,line] = [int(x) for x in input().split(" ")]
+	if direction:	#세로일 경우
+		for j in range(N):
+			lamp[j][line-1] += 1
+	else:	#가로일 경우
+		for j in range(M):
+			lamp[line-1][j] += 1
+				
+for i in range(N):
+	for j in range(M):
+		print(lamp[i][j] % 2, end = " ")
+	print()
