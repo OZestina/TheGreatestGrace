a82b2f02817f0fd6acf8df2a2af26c8a05556d8b
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-21 23:16:05 +0900

    Update 1122_전등비버.py

--- codingTest/goorm/py/1122_전등비버.py
+++ codingTest/goorm/py/1122_전등비버.py
@@ -21,3 +21,61 @@ for i in range(N):
 	for j in range(M):
 		print(lamp[i][j] % 2, end = " ")
 	print()
+
+	
+#2nd try
+#중복되는 입력값 빼주기
+#그래도 5,8번 runtime error (제한시간 1초)
+[N,M] = [int(x) for x in input().split(" ")]
+lamp = []
+for i in range(N):
+	lamp.append([int(x) for x in input().split(" ")])
+	
+onoff = []
+times = int(input())
+for i in range(times):
+	[direction,line] = [int(x) for x in input().split(" ")]
+	if [direction,line] in onoff:
+		onoff.remove([direction,line])
+	else:
+		onoff.append([direction,line])
+
+for i in onoff:
+	if i[0]:	#세로일 경우
+		for j in range(N):
+			lamp[j][i[1]-1] += 1
+	else:	#가로일 경우
+		for j in range(M):
+			lamp[i[1]-1][j] += 1
+				
+for i in range(N):
+	for j in range(M):
+		print(lamp[i][j] % 2, end = " ")
+	print()
+
+
+#3rd try
+#배열대신 딕셔너리써보기
+#그래도 5,8번 runtime error (제한시간 1초)
+
+[N,M] = [int(x) for x in input().split(" ")]
+lamp = dict()
+for i in range(N):
+	temp = [int(x) for x in input().split(" ")]
+	for j in range(M):
+		lamp[i*M+j] = temp[j]
+
+times = int(input())
+for i in range(times):
+	[direction,line] = [int(x) for x in input().split(" ")]
+	if direction:	#세로일 경우
+		for j in range(N):
+			lamp[j*M+line-1] += 1
+	else:	#가로일 경우
+		for j in range(M):
+			lamp[(line-1)*N+j] += 1
+				
+for i in range(N):
+	for j in range(M):
+		print(lamp[i*M + j] % 2, end = " ")
+	print()
