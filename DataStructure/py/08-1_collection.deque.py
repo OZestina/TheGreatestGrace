323c98bef363b84ed36b53c3c53f3b33a36f24b8
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-23 22:39:14 +0900

    Create 08-1_collection.deque.py

---
+++ DataStructure/py/08-1_collection.deque.py
@@ -0,0 +1,47 @@
+#2021.11.23
+
+# 2021.11.23
+
+from typing import Any
+from collections import deque
+
+class Stack:
+
+    def __init__(self, maxlen: int = 256) -> None:
+        self.capacity = maxlen
+        self.__stk = deque([], maxlen)
+
+    def __len__(self) -> int:
+        return len(self.__stk)
+
+    def is_empty(self) -> bool:
+        return not self.__stk
+
+    def is_full(self) -> bool:
+        return len(self.__stk) == self.__stk.maxlen
+
+    def push(self, value: Any) -> None:
+        self.__stk.append(value)
+
+    def pop(self) -> Any:
+        return self.__stk.pop()
+
+    def peek(self) -> Any:
+        return self.__stk[-1]
+
+    def clear(self) -> None:
+        self.__stk.clear()
+
+    def find(self, value: Any) -> Any:
+        try: return self.__stk.index(value)
+        except ValueError:
+            return -1
+
+    def count(self, value: Any) -> int:
+        return self.__stk.count(value)
+
+    def __contains__(self, value: Any) -> bool:
+        return self.count(value) > 0
+
+    def dump(self) -> int:
+        print(list(self.__stk))
