9145becc42f9749dec49221d656721270e86a433
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-26 00:49:09 +0900

    Update 1123_시간복잡도.py

--- codingTest/goorm/py/1123_시간복잡도.py
+++ codingTest/goorm/py/1123_시간복잡도.py
@@ -1,5 +1,19 @@
 # https://level.goorm.io/exam/49098/%EC%8B%9C%EA%B0%84%EB%B3%B5%EC%9E%A1%EB%8F%84/quiz/1
 
+#5th try
+#필요없는 if문 없애자..
+def howManyZeros(n):
+	count5 = 0
+	while n >= 5:
+		count5 += n//5
+		n = n//5
+	return count5
+
+factorial = int(input())
+print(howManyZeros(factorial))
+
+#=============================================================================
+
 #1st try
 #5만 카운트해서 출력하는 방식 사용 (2*5 = 10이고, 2는 5보다 당연히 많을거니까)
 #수가 커지면 시간초과로 에러 발생
