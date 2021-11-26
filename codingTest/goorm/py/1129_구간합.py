6a9a4335e467f384a7aaf596e4bd8ec2ff109d00
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-11-26 13:30:30 +0900

    Create 1129_구간합.py

---
+++ codingTest/goorm/py/1129_구간합.py
@@ -0,0 +1,11 @@
+# https://level.goorm.io/exam/43263/%ED%8A%B9%EC%A0%95-%EA%B5%AC%EA%B0%84%EC%9D%98-%ED%95%A9/quiz/1
+
+user_input = input()
+array = [int(x) for x in input().split(" ")]
+[start, end] = [int(x) for x in input().split(" ")]
+
+partial = 0
+for i in range(start-1, end):
+	partial += array[i]
+	
+print(partial)
