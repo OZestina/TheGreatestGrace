b7b493c51fc5f0bc2070d48599994a74d0d07412
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-12-09 00:26:31 +0900

    Update 10_recursion.py

--- DataStructure/py/10_recursion.py
+++ DataStructure/py/10_recursion.py
@@ -26,6 +26,7 @@ def recur(n: int) -> int:
 
 recur(4)        #1 2 3 1 4 1 2
 
+
 #5-4)비재귀적으로 재귀 함수 구현 (while문)
 def recur2(n: int) -> int:
     while n > 0:
@@ -34,6 +35,7 @@ def recur2(n: int) -> int:
         n = n-2
 
 recur2(4)        #1 2 3 1 4 1 2
+
 
 #5-5) 스택으로 재귀함수 구현
 from collections import deque
@@ -52,3 +54,49 @@ def recur3(n: int) -> int:
         break
 
 recur3(4)       #1 2 3 1 4 1 2
+
+
+#5-6) 하노이의 탑 구현
+#no는 원반 개수, x는 시작기둥, y는 목표기둥
+def move(no: int, x: int, y: int) -> None:
+    if no > 1:
+        move(no-1, x, 6-x-y)
+    print(f'원반 [{no}]을(를) {x}기둥에서 {y}기둥으로 옮깁니다')
+    if no > 1:
+        move(no-1, 6-x-y, y)
+
+move(3,1,3)
+
+
+#5-9) 8퀸문제
+pos = [0]*8
+flag_a = [False] * 8  #각 행 퀸 배치여부 확인
+flag_b = [False] * 15  #대각선방향에 퀸 배치여부 확인
+flag_c = [False] * 15  #대각선방향에 퀸 배치여부 확인
+
+
+def put() -> None:  #출력
+    # for i in range(8):
+    #     print(f'{pos[i]:2}', end='')
+    # print()
+    for j in range(8):
+        for i in range(8):
+            print('■' if pos[i]==j else '□', end='')
+        print()
+    print()
+
+
+def set(i:int) -> None: #i열에 퀸 배치
+    for j in range(8):
+        if (    not flag_a[j]
+            and not flag_b[i+j]
+            and not flag_c[i-j+7]):
+            pos[i] = j
+            if i == 7:
+                put()
+            else:
+                flag_a[j] = flag_b[i+j] = flag_c[i-j+7] = True
+                set(i+1)
+                flag_a[j] = flag_b[i+j] = flag_c[i-j+7] = False
+
+set(0)
