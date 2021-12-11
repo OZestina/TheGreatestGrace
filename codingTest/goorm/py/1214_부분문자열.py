786451905d3941fcd9c9b4e14a457f5cefe0be39
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-12-12 04:09:50 +0900

    Create 1214_부분문자열.py

---
+++ codingTest/goorm/py/1214_부분문자열.py
@@ -0,0 +1,8 @@
+# https://level.goorm.io/exam/43257/%EB%B6%80%EB%B6%84%EB%AC%B8%EC%9E%90%EC%97%B4/quiz/1
+
+user_input = input()
+
+toPrint = ''
+for letter in user_input:
+	toPrint += letter
+	print(toPrint)
