f73e2ef8a4acb9fff1af5b689c47c5d11bdfb9e4
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-12-14 11:43:52 +0900

    Create *1214_키패드.py

---
+++ codingTest/programmers/py/*1214_키패드.py
@@ -0,0 +1,31 @@
+# https://programmers.co.kr/learn/courses/30/lessons/67256?language=python3
+
+#테스트케이스도 통과 못함ㅎ
+#2580을 어떻게 처리할지... (특히 0)
+# 좀 더 생각해보자
+def solution(numbers, hand):
+    answer = ''
+    left = right = 0
+    for number in numbers:
+        if number in [1,4,7]:
+            answer += 'L'
+            left = number+2   #right과 보폭맞춰!
+        elif number in [3,6,9]:
+            answer += 'R'
+            right = number
+        else:
+            if abs(number-left) == abs(number-right):
+                if hand == 'left':
+                    answer += 'L'
+                    left = number
+                else:
+                    answer += 'R'
+                    right = number
+            elif abs(number-left) > abs(number-right):
+                answer += 'R'
+                right = number
+            else:
+                answer += 'L'
+                left = number
+        
+    return answer
