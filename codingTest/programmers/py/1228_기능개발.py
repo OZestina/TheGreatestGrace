ebc7517c5635ebe2d7eced60af1a021bc3db0457
Zestina OH <72402916+OZestina@users.noreply.github.com>
2021-12-28 23:58:59 +0900

    Update and rename *1228_.py to 1228_기능개발.py

---
+++ codingTest/programmers/py/1228_기능개발.py
@@ -0,0 +1,30 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42586
+
+
+# 1st try
+# 머릿속으로 이렇게 이렇게 하면 되지 않을까? 해서 해본게 1트만에 성공!
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    process = 1
+    day = -1
+    for process_num in range(len(progresses)-1):
+        if day == -1:
+            day = math.ceil((100 - progresses[process_num]) / speeds[process_num])
+            process = 1
+        elif speeds[process_num] * day + progresses[process_num] >= 100:
+            process += 1
+        else:
+            answer.append(process)
+            process = 1
+            day = math.ceil((100 - progresses[process_num]) / speeds[process_num])
+    if speeds[len(speeds)-1] * day + progresses[len(speeds)-1] >= 100:
+        answer.append(process+1)
+    else:
+        answer.append(process)
+        answer.append(1)
+    return answer
+  
+  
+#2nd try
