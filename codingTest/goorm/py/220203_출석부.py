470860fb232a93b74ff8a2253c6eec74f7fc4cb8
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-02-03 23:56:41 +0900

    Update 220203_출석부.py

--- codingTest/goorm/py/220203_출석부.py
+++ codingTest/goorm/py/220203_출석부.py
@@ -1,1 +1,9 @@
 # https://level.goorm.io/exam/49069/%EC%B6%9C%EC%84%9D%EB%B6%80/quiz/1
+
+name = input()
+for idx in range(0,len(name)-1):
+	if ord(name[idx]) > ord(name[idx+1]):
+		print(name[:idx]+name[idx+1:])
+		break
+else:
+	print(name[:len(name)-1])
