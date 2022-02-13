e2b44ab9bc380e3e326fae1e5dba33d34f13c90d
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-02-13 23:55:42 +0900

    Update 220212_행성관측.py

--- codingTest/goorm/py/220212_행성관측.py
+++ codingTest/goorm/py/220212_행성관측.py
@@ -1,1 +1,2 @@
 # https://level.goorm.io/exam/49082/%ED%96%89%EC%84%B1%EC%9D%84-%EA%B4%80%EC%B8%A1%ED%95%98%EB%8A%94-%ED%95%A0%EC%95%84%EB%B2%84%EC%A7%80%EC%9D%98-%EC%9D%B4%EC%95%BC%EA%B8%B0/quiz/1
+# 생각한거보다 어렵네..?
