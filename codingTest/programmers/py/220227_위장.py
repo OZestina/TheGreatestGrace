86255e20fa3c4540af3f81df32c5cf98625ea037
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-02-28 01:26:58 +0900

    Update 220227_위장.py

--- codingTest/programmers/py/220227_위장.py
+++ codingTest/programmers/py/220227_위장.py
@@ -1,1 +1,40 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42578?language=python3
+
+# 1st try
+# combinations과 reduce 익명함수로 시도
+# 1번 Timeout으로 실패
+from itertools import combinations
+from functools import reduce
+
+def solution(clothes):
+    closet = dict()
+    for item in clothes:
+        if item[1] in closet:
+            closet[item[1]] += 1
+        else:
+            closet[item[1]] = 1
+    
+    answer = 0
+    for i in range(1,len(closet.values())+1):
+        for combination in combinations(list(closet.values()),i):
+            answer += reduce(lambda x,y: x*y, combination)
+            
+    return answer
+  
+  
+# 2nd try
+# 전혀 다른 방식으로 접근했어야 했다
+# 각 항목 당 0~n개의 아이템을 착용하는 경우의 수를 곱셈으로 구하고, 모두 착용하지 않는 하나의 수를 빼주는게 제일 빠름...
+def solution(clothes):
+    closet = dict()
+    for item in clothes:
+        if item[1] in closet:
+            closet[item[1]] += 1
+        else:
+            closet[item[1]] = 1
+    
+    answer = 1
+    for value in closet.values():
+        answer *= value+1
+    
+    return answer-1
