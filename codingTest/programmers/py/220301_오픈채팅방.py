b0269d7959521e1473967655cd0ae7e473528ac4
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-03-02 17:42:13 +0900

    Update 220301_오픈채팅방.py

--- codingTest/programmers/py/220301_오픈채팅방.py
+++ codingTest/programmers/py/220301_오픈채팅방.py
@@ -30,3 +30,24 @@ def solution(record):
         answer.append(result)
     
     return answer
+
+
+# 2nd try (다른 사람 풀이 참고)
+# 코드 라인을 더 짧게 줄일 수 있는데, 걸리는 시간 측면에서는 위의 것이 나음
+# 시간을 줄이고 싶다면 메모리를 더 써보자
+def solution(record):
+    nick = dict()   #uid마다 최종 닉네임 저장용
+    comment = {"Enter": "님이 들어왔습니다.", "Leave": "님이 나갔습니다."}
+    answer = []
+    
+    for i in record:
+        temp = i.split()
+        if i[0] == 'E' or i[0] == 'C':     #Enter or Change
+            nick[temp[1]] = temp[2]
+    
+    for i in record:
+        temp = i.split()
+        if temp[0] != "Change":
+            answer.append(nick[temp[1]] + comment[temp[0]])
+    
+    return answer
