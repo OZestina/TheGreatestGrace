cb95608d1c40663d3c0fb25a8ddf6fd7f0637993
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-04-06 02:08:17 +0900

    Update 220405_피보나치.py

--- codingTest/programmers/py/220405_피보나치.py
+++ codingTest/programmers/py/220405_피보나치.py
@@ -9,3 +9,19 @@ def solution(n):
     
     
 # 2nd try
+# 재귀포기, for문으로 O(n) 복잡도 코드로 작성 => 성공
+def solution(n):
+    F = [0,1] + [0]*(n-1)
+    for i in range(2, n+1):
+        F[i] = (F[i-1] + F[i-2]) % 1234567
+    return F[n]
+
+
+
+# 3rd try: 다른 사람의 풀이 참고
+# 맞다 맞다 for문 없이 이렇게 할 수도 있었지!
+def solution(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, (a+b)%1234567
+    return a
