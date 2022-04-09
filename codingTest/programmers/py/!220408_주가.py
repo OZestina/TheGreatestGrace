c0f29e7fe402fd9f534d9b2aea17c4607ad4fc79
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-04-09 23:51:13 +0900

    Update !220408_주가.py

--- codingTest/programmers/py/!220408_주가.py
+++ codingTest/programmers/py/!220408_주가.py
@@ -14,3 +14,21 @@ def solution(prices):
         answer.append(count)
     
     return answer + [0]
+
+
+# 2nd try
+# 그냥 이중for문도 통과되는 거였다...
+def solution(prices):
+    answer = [0] * len(prices)
+    
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]: answer[i] += 1
+            else: 
+                answer[i] += 1
+                break
+            
+    return answer
+
+
+# 3rd try
