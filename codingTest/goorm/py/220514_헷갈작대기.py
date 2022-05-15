2a0f23cc280f5c759b874f42072b2692db16fdbd
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-05-15 23:00:16 +0900

    Update and rename *220514_.py to 220514_헷갈작대기.py

---
+++ codingTest/goorm/py/220514_헷갈작대기.py
@@ -0,0 +1,8 @@
+# https://level.goorm.io/exam/47882/%ED%97%B7%EA%B0%88%EB%A6%AC%EB%8A%94-%EC%9E%91%EB%8C%80%EA%B8%B0/quiz/1
+
+from collections import Counter
+user_input = Counter(input())
+print(user_input['1'])
+print(user_input['I'])
+print(user_input['l'])
+print(user_input['|'])
