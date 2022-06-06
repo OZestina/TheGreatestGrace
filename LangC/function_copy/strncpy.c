a8724cdcfd14cd72e1e70e61cb7c41781cacd410
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-06-06 13:21:44 +0900

    Update and rename *strncpy.c to strncpy.c

---
+++ LangC/function_copy/strncpy.c
@@ -0,0 +1,17 @@
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	idx;
+
+	idx = 0;
+	while (src[idx] != '\0' && idx < n)
+	{
+		dest[idx] = src[idx];
+		idx++;
+	}
+	while (idx < n)
+	{
+		dest[idx] = '\0';
+		idx++;
+	}
+	return (dest);
+}
