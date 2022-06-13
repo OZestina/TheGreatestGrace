81ad2fc5bc3a543c1d726a35e0bc62cece18f1a1
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-06-13 23:59:35 +0900

    Update 220613_의좋은형제.py

--- codingTest/goorm/py/220613_의좋은형제.py
+++ codingTest/goorm/py/220613_의좋은형제.py
@@ -1,2 +1,16 @@
 # https://level.goorm.io/exam/49088/%EC%9D%98%EC%A2%8B%EC%9D%80-%ED%98%95%EC%A0%9C/quiz/1
 
+jin, sun = [int(i) for i in input().split()]
+days = int(input())
+
+for i in range(days):
+	if i % 2 == 0:
+		temp = jin - int(jin / 2)
+		jin -= temp
+		sun += temp
+	else:
+		temp = sun - int(sun / 2)
+		jin += temp
+		sun -= temp
+
+print(jin, sun)
