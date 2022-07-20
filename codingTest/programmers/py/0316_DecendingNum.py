9d10266a57ef69e0758684ec00e4abadfb2c2957
Zestina OH <72402916+OZestina@users.noreply.github.com>
2022-07-21 00:00:18 +0900

    Update and rename programmers/codingTest/programmers/py/0316_DecendingNum.py to codingTest/programmers/py/0316_DecendingNum.py

--- codingTest/programmers/py/0316_DecendingNum.py
+++ codingTest/programmers/py/0316_DecendingNum.py
@@ -1,11 +1,8 @@
 #https://programmers.co.kr/learn/courses/30/lessons/12933?language=python3
 
-#1st try
+#1st try => 성공
 #int를 str으로 변환 후 내림차순 정렬, 그 후 다시 int로 변환!
 def solution(n):
     a = ''.join(sorted(str(n),reverse=True))
     answer = int(a)
     return answer
-  
-#2nd try
-to be continued...
