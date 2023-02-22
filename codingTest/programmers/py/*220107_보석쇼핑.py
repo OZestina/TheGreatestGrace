eacac34a1a165ff130bf952c4d31299681c36858
Zestina OH <72402916+OZestina@users.noreply.github.com>
2023-02-22 11:25:24 +0900

    Update *220107_보석쇼핑.py

--- codingTest/programmers/py/*220107_보석쇼핑.py
+++ codingTest/programmers/py/*220107_보석쇼핑.py
@@ -1,8 +1,7 @@
 # https://programmers.co.kr/learn/courses/30/lessons/67258
 
-# 1st try
+# 1st try (시간초과 fail)
 # 최소 길이부터 모든 경우의 수를 다 훑는 방법으로 해봄
-# 시간초과로 fail
 def solution(gems):
     gem_len = len(set(gems))
     length = gem_len
@@ -15,15 +14,30 @@ def solution(gems):
         length += 1
     return answer
   
-# 2nd try
-# 각 보석의 개수를 세서 1인 애들을 무조건 포함하는 리스트로 시작해야하는걸까..?
 
+# 2nd try (23.02.22) (효율성 fail)
+from collections import defaultdict
 
 def solution(gems):
-    gem_list = dict()
-    count = 0
-    for gem in gems:
-        if gem in gem_list:
-            gem_list[gem] = gem_list[gem] + [count]
-        else: gem_list[gem] = [count]
-        count += 1
+    answer = []
+    
+    jewel = defaultdict(list)
+    for i in range(len(gems)):
+        jewel[gems[i]] += [i]
+    
+    for i in range(len(gems)):
+        end = i
+        for j, value in jewel.items():
+            if value[-1] < i:
+                end = -1
+                break
+            for v in value:
+                if i <= v:
+                    if end < v:
+                        end = v
+                    break
+        if i <= end:
+            answer.append([i, end])
+        
+    temp = min(answer, key=lambda x: x[1] - x[0])
+    return [temp[0] + 1, temp[1] + 1]
