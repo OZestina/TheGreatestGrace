5b7b5c7f146bb7e06c375e07531699383766d962
Zestina OH <72402916+OZestina@users.noreply.github.com>
2023-04-03 14:23:12 +0900

    Update 1101_완주못선수.py

--- codingTest/programmers/py/1101_완주못선수.py
+++ codingTest/programmers/py/1101_완주못선수.py
@@ -34,3 +34,42 @@ def solution(participant, completion):
         temp -= int(hash(com))
     
     return dic[temp]
+
+
+
+# (0318 진행내용) ==============================================
+#1st try
+#틀린 코드는 아닌데, 효율성 테스트 모두 fail...
+def solution(participant, completion):
+    for i in participant:
+        if i in completion:
+            completion.remove(i)
+        else:
+            answer = i
+    return answer
+  
+#2nd
+#또 효율성 테스트 모두 fail...
+def solution(participant, completion):
+    for i in participant:
+        try:
+            completion.remove(i)
+        except:
+            answer = i
+    return answer
+
+#2-1 try
+def solution(participant, completion):
+    for i in completion:
+        participant.remove(i)
+        answer = participant[0]
+    return answer
+  
+#3rd try
+#교집합을 이용해서 풀어봤는데, 이건 중복값을 모두 날려버려서 실패.
+def solution(participant, completion):
+    answer = list (set(participant) - set(completion))
+    return answer[0]
+
+#4th try - working on
+#카테고리가 '해시'인데, 이걸로 풀어보자....
