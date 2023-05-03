3a8896f7ce501fe6cf099cf20e86e971e9b82c25
Zestina OH <72402916+OZestina@users.noreply.github.com>
2023-05-03 23:59:22 +0900

    Delete 220806_SkillTest(L3)_1.py

--- codingTest/programmers/py/test/220806_SkillTest(L3)_1.py
+++
@@ -1,9 +0,0 @@
-# https://programmers.co.kr/skill_checks/405002?challenge_id=2393
-
-def solution(triangle):
-    for i in range(1, len(triangle)):
-        triangle[i][0] += triangle[i-1][0]
-        triangle[i][i] += triangle[i-1][i-1]
-        for j in range(1, i):
-            triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
-    return max(triangle[len(triangle)-1])
