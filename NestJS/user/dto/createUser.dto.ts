beb847ca9254a564cf56c4f605dd6fddff608381
Zestina OH <72402916+OZestina@users.noreply.github.com>
2023-09-03 21:44:14 +0900

    Update createUser.dto.ts

--- NestJS/user/dto/createUser.dto.ts
+++ NestJS/user/dto/createUser.dto.ts
@@ -2,10 +2,18 @@ import { IsNotEmpty } from "class-validator";
 
 export class CreateUserDto {
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLenght(10)
     username: string;
 
+    @IsString()
+    @Matches(정규식, {
+        message: 'file name not valid'
+    })
     profileurl: string;
 
     @IsNotEmpty()
+    @IsBoolean()
     require2fa: boolean;
 }
